d45518bb9aac1f0015b297537558bfad1837b8dd
Hc <henrique.c.oliveira@gmail.com>
2024-11-13 21:14:39 -0300

    feat: add transaction type bagde

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,26 @@
-const TranscripitionPage = () => {
-  return <h1>Transcrition page</h1>;
+import { ArrowDownUpIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { db } from "../_lib/prisma";
+import { DataTable } from "../_components/ui/data-table";
+import { transactionColumns } from "./_columns";
+
+const TranscripitionPage = async () => {
+  //acessar as transações do banco de dados
+
+  const transactions = await db.transaction.findMany({});
+  return (
+    <div className="space-y-6 p-6">
+      {/* Titulo e Botao */}
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <Button className="rounded-full">
+          Adicionar Transação
+          <ArrowDownUpIcon />
+        </Button>
+      </div>
+      <DataTable columns={transactionColumns} data={transactions} />
+    </div>
+  );
 };
 
 export default TranscripitionPage;
