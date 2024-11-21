ca35daa85e45d0edcf02b3b998597011b5286cea
Hc <henrique.c.oliveira@gmail.com>
2024-11-21 19:44:10 -0300

    feat: ensure transiction and subscription pages are protected

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -3,11 +3,22 @@ import { DataTable } from "../_components/ui/data-table";
 import { transactionColumns } from "./_columns";
 import AddTrasanctionButton from "../_components/add-transaction-button";
 import Navbar from "../_components/navbar";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const TranscripitionPage = async () => {
-  //acessar as transações do banco de dados
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
 
-  const transactions = await db.transaction.findMany({});
+  //acessar as transações do banco de dados
+  const transactions = await db.transaction.findMany({
+    where: {
+      userId,
+    },
+  });
+
   return (
     <>
       <Navbar />
