1633bef5250d9d18642ae5805008d008213e24d9
Hc <henrique.c.oliveira@gmail.com>
2024-11-26 19:31:38 -0300

    feat: add transactions pie chart component

---
+++ app/(home)/_components/transaction-pie-charts.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import { Pie, PieChart } from "recharts";
+
+import { Card, CardContent } from "@/app/_components/ui/card";
+
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/app/_components/ui/chart";
+import { TransactionType } from "@prisma/client";
+import { TransactionPercentagePerType } from "@/app/_data/get-dashboard/types";
+import { PiggyBankIcon, TrendingDownIcon, TrendingUpIcon } from "lucide-react";
+import PercentageItem from "./pecentage-item";
+
+const chartConfig = {
+  [TransactionType.INVESTMENT]: {
+    label: "Investido",
+    color: "#FFFFFF",
+  },
+  [TransactionType.DEPOSIT]: {
+    label: "Receita",
+    color: "#55b02e",
+  },
+  [TransactionType.EXPENSE]: {
+    label: "Despesas",
+    color: "#e93030",
+  },
+} satisfies ChartConfig;
+
+interface TransactionsPieChartProps {
+  typesPercentage: TransactionPercentagePerType;
+  balance: number;
+  depositsTotal: number;
+  investmentsTotal: number;
+  expensesTotal: number;
+}
+
+const TransacitionsPieChart = ({
+  depositsTotal,
+  investmentsTotal,
+  expensesTotal,
+  typesPercentage,
+}: TransactionsPieChartProps) => {
+  const chartData = [
+    {
+      type: TransactionType.DEPOSIT,
+      amount: depositsTotal,
+      fill: "#55b02e",
+    },
+    {
+      type: TransactionType.INVESTMENT,
+      amount: investmentsTotal,
+      fill: "#ffffff",
+    },
+    {
+      type: TransactionType.EXPENSE,
+      amount: expensesTotal,
+      fill: "#e93030",
+    },
+  ];
+
+  return (
+    <Card className="flex flex-col">
+      <CardContent className="flex-1 p-12 pb-0">
+        <ChartContainer
+          config={chartConfig}
+          className="mx-auto aspect-square max-h-[250px]"
+        >
+          <PieChart>
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Pie
+              data={chartData}
+              dataKey="amount"
+              nameKey="type"
+              innerRadius={60}
+            />
+          </PieChart>
+        </ChartContainer>
+
+        <div className="space-y-3">
+          <PercentageItem
+            icon={<TrendingUpIcon size={16} className="text-primary" />}
+            title="Receita"
+            value={typesPercentage[TransactionType.DEPOSIT]}
+          />
+
+          <PercentageItem
+            icon={<TrendingDownIcon size={16} className="text-red-500" />}
+            title="Despesas"
+            value={typesPercentage[TransactionType.EXPENSE]}
+          />
+
+          <PercentageItem
+            icon={<PiggyBankIcon size={16} />}
+            title="Investido"
+            value={typesPercentage[TransactionType.INVESTMENT]}
+          />
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default TransacitionsPieChart;
